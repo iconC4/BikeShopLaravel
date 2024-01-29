095d091097c592feebf0ce876966f422fd498a72
iconC4 <cfour2545@gmail.com>
2024-01-29 23:52:11 +0700

    Update Now

---
+++ app/Http/Controllers/OrderdetailController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use Illuminate\Http\Request;
+
+class OrderdetailController extends Controller
+{
+    //
+
+}
